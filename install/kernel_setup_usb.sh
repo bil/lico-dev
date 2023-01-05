691e0867b55e74a994725b0973fea7deae498e15
Sabar Dasgupta <s@bardasgupta.com>
2023-01-05 17:35:04 -0500

    update kernel install for automatic debian-install

--- install/kernel_setup_usb.sh
+++ install/kernel_setup_usb.sh
@@ -8,7 +8,7 @@ set -ev
 
 # set some variables
 INSTALL_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-KERNEL_DIR=`pwd`/../rt_kernel
+KERNEL_DIR=~/.licorice/rt_kernel
 TMP_DIR=/tmp
 
 NUM_CPUS=`grep processor /proc/cpuinfo|wc -l`
@@ -16,17 +16,14 @@ NUM_CPUS=`grep processor /proc/cpuinfo|wc -l`
 # update to most recent version of packages, install essentials, do some cleanup
 sudo apt-get update
 sudo apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" upgrade
-sudo apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install libncurses5-dev build-essential libssl-dev kernel-package dwarves libelf-dev flex bison openssl dkms libelf-dev libudev-dev libpci-dev libibverty-dev autoconf fakeroot
+sudo apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install libncurses5-dev build-essential libssl-dev kernel-package dwarves libelf-dev flex bison openssl dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf fakeroot
 sudo apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" autoremove
 
 # download kernel and rt-patch if not exists
 cd $TMP_DIR
-# wget https://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-stable-rt.git/snapshot/linux-stable-rt-5.4.209-rt77.tar.gz
-
-#cd KERNEL DIR
-wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.4.221.tar.gz
-wget https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.4/patch-5.4.221-rt79.patch.gz
-zcat ../patch-5.4.221-rt79.patch.gz | patch -p1
+#wget https://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-stable-rt.git/snapshot/linux-stable-rt-5.4.209-rt77.tar.gz
+wget https://www.kernel.org/pub/linux/kernel/v5.x/linux-5.4.221.tar.gz
+wget https://www.kernel.org/pub/linux/kernel/projects/rt/5.4/patch-5.4.221-rt79.patch.gz
 
 #
 # to check signature
@@ -42,11 +39,12 @@ zcat ../patch-5.4.221-rt79.patch.gz | patch -p1
 rm -rf $KERNEL_DIR
 mkdir -p $KERNEL_DIR
 cd $KERNEL_DIR
-tar -zxvf $TMP_DIR/linux-stable-rt-5.4.209-rt77.tar.gz
+tar -zxvf $TMP_DIR/linux-5.4.221.tar.gz
+cd linux-5.4.221
+zcat /tmp/patch-5.4.221-rt79.patch.gz | patch -p1
 
 # copy kernel .config file from git
 cp $INSTALL_DIR/.config $KERNEL_DIR/linux-stable-rt-5.4.209-rt77/.config
-
 # examine config
 # sudo apt-get install flex bison
 # make menuconfig
@@ -60,8 +58,8 @@ make-kpkg clean
 CONCURRENCY_LEVEL=$NUM_CPUS fakeroot make-kpkg --initrd --append-to-version=-licorice binary
 
 # install kernel
-sudo dpkg --force-confdef --force-confnew -i $KERNEL_DIR/linux-image-5.4.209-licorice-rt77_5.4.209-licorice-rt77-10.00.Custom_amd64.deb
-sudo dpkg --force-confdef --force-confnew -i $KERNEL_DIR/linux-headers-5.4.209-licorice-rt77_5.4.209-licorice-rt77-10.00.Custom_amd64.deb
+sudo dpkg --force-confdef --force-confnew -i $KERNEL_DIR/linux-image-5.4.221-licorice-rt79_5.4.221-licorice-rt79-10.00.Custom_amd64.deb
+sudo dpkg --force-confdef --force-confnew -i $KERNEL_DIR/linux-headers-5.4.221-licorice-rt79_5.4.221-licorice-rt79-10.00.Custom_amd64.deb
 
 # notify reboot when done
 printf "\n\n%s\n" "Kernel installation complete. Please reboot the system."
